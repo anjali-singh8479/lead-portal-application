cb14d5556df1f2b8539c02016ebad71c192b9514
anjali-singh8479 <shyanj05@gmail.com>
2026-06-13 13:03:44 +0530

    created a basic form using react

---
+++ frontend/src/App.js
@@ -0,0 +1,148 @@
+
+
+
+import { useState } from "react";
+
+function App() {
+
+  const formStyle = {
+  width: "500px",
+  margin: "auto",
+  marginTop: "50px",
+  padding: "20px",
+  border: "1px solid #ccc",
+  borderRadius: "10px",
+  alignItems: "left",
+  display: "flex",
+  flexDirection: "column"
+};
+
+  const [formData, setFormData] = useState({
+    fullName: "",
+    email: "",
+    phone: "",
+    experience: "",
+    skills: ""
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = (e) => {
+  e.preventDefault();
+
+  if (
+    !formData.fullName ||
+    !formData.email ||
+    !formData.phone
+  ) {
+    alert("Please fill required fields");
+    
+    return;
+  }
+
+
+
+  alert("Application Submitted");
+  setFormData({         
+      fullName: "",
+      email: "",
+      phone: "",
+      skills: "",
+      experience: ""
+    });
+};
+
+  return (
+    <div style={formStyle}>
+      <h1>Lead Application Portal</h1>
+
+      <form onSubmit={handleSubmit}>
+
+        <div>
+          <label>Full Name</label>
+          <br />
+          <input
+            type="text"
+            name="fullName"
+            value={formData.fullName}
+            onChange={handleChange}
+          />
+        </div>
+
+        <br />
+
+        <div>
+          <label>Email</label>
+          <br />
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+        </div>
+
+        <br />
+
+        <div>
+          <label>Phone Number</label>
+          <br />
+          <input
+            type="text"
+            name="phone"
+            value={formData.phone}
+            onChange={handleChange}
+          />
+        </div>
+
+        <br />
+
+        <div>
+          <label>Experience(In Yrs)</label>
+          <br />
+          <input
+            type="number"
+            name="experience"
+            value={formData.experience}
+            onChange={handleChange}
+          />
+        </div>
+
+        <br />
+
+        <div>
+          <label>Skills</label>
+          <br />
+          <textarea
+            name="skills"
+            value={formData.skills}
+            onChange={handleChange}
+          />
+        </div>
+
+        <br />
+
+        <button type="submit" style={{
+    backgroundColor: "#2563eb",
+    color: "white",
+    padding: "10px 20px",
+    border: "none",
+    borderRadius: "5px",
+    cursor: "pointer",
+    fontSize: "16px"
+  }}>
+          Submit
+        </button>
+
+      </form>
+    </div>
+  );
+}
+
+export default App;
+
