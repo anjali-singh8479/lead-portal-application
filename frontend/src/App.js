78a34700e72a75c51355952062c4a1d190c49bd1
anjali-singh8479 <shyanj05@gmail.com>
2026-06-13 14:29:20 +0530

    Zoho Oauth and refreah token generated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -50,7 +50,7 @@ const handleSubmit = async (e) => {
   try {
 
     const response = await fetch(
-      "http://localhost:3000/createlead",
+    "http://localhost:3000/createlead",
       {
         method: "POST",
         headers: {
